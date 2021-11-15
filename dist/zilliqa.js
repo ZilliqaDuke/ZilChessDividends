9f1f8601320656cadbbca65f59471429778f0610
ZilliqaDuke <76908890+ZilliqaDuke@users.noreply.github.com>
2021-11-15 09:25:33 +0400

    Delete dist directory

--- dist/zilliqa.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.zilliqa = void 0;
-const zilliqa_1 = require("@zilliqa-js/zilliqa");
-exports.zilliqa = new zilliqa_1.Zilliqa('https://api.zilliqa.com/');
-//# sourceMappingURL=zilliqa.js.map
